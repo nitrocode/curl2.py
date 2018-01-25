b08e9a92f6a1d556e20bb9f1b501cda174ff00b3
nitrocode <nitrocode@github.com>
2018-01-25 01:03:13 -0500

    Python2 and 3 compatibility, pep8, and more doc strings

--- curl2.py
+++ curl2.py
@@ -6,45 +6,70 @@ import shlex
 
 INDENT = 4
 
+def dict_to_code(name, simple_dict):
+    """Converts a dictionary to a python compatible key value pair
 
-def create_request(url, data, method, cookies, headers):
+    >>> code = dict_to_code("cookies", cookies)
+
+    :param name: name of the variable
+    :param simple_dict: dictionary to iterate
+    :return: python compatible code in a list
+    """
+    code = []
+    if simple_dict:
+        code.append("{} = {{".format(name))
+        # check for python3
+        try:
+            for k, v in simple_dict.items():
+                code.append("{i}'{k}': '{v}',".format(i=" " * INDENT, k=k, v=v))
+        except:
+            for k, v in simple_dict.iteritems():
+                code.append("{i}'{k}': '{v}',".format(i=" " * INDENT, k=k, v=v))
+        code.append("}\n")
+    return code
+
+
+def create_request(url, method, cookies, headers, data=None):
     """Create request code from params
+
+    >>> code = create_request("https://localhost:8080", None, "get", None,
+    None)
 
     :param url: url requested
     :param method: method used e.g. get, post, delete, put
     :param cookies: dict of each cookie
     :param headers: dict of each header
+    :param data: optional param to provided data to the request
+    :return: python compatible code in a list
     """
-    pycode = []
-    # check for a cookie
-    if cookies:
-        pycode.append("cookies = {")
-        for k, v in cookies.iteritems():
-            pycode.append("{I}'{k}': '{v}'".format(I=" " * INDENT, k=k, v=v))
-        pycode.append("}\n")
-    # assumes there are headers
-    pycode.append("headers = {")
-    for k, v in headers.iteritems():
-        pycode.append(" " * INDENT + "'{k}': '{v}',".format(k=k, v=v))
-    pycode.append("}\n")
-    pycode.append("url = '{0}'".format(url))
-    # if there is a cookie, then attach it to the requests call
-    resstr = "res = requests.{method}(url, ".format(method=method)
+    code = []
+    key_value = "{i}'{k}': '{v}'"
+    # check for cookies
+    code += dict_to_code("cookies", cookies)
+    # check for headers
+    code += dict_to_code("headers", headers)
+    code.append("url = '{}'".format(url))
+    resstr = "res = requests.{}(url, ".format(method)
     append = "headers=headers"
+    # if there are cookies / data, then attach it to the requests call
     if cookies:
         append += ", cookies=cookies"
     if data:
-        pycode.append("data = '{0}'".format(data))
+        code.append("data = '{}'".format(data))
         append += ", data=data"
-    pycode.append(resstr + append + ")")
-    pycode.append("print res.content\n")
-    return pycode
+    code.append(resstr + append + ")")
+    code.append("print(res.content)\n")
+    return code
 
 
-def curlToPython(command):
+def curl_to_python(command):
     """Convert curl command to python script.
 
+    >>> code = curl_to_python(command)
+    >>> print('\n'.join(code))
+
     :param command: curl command exported from Chrome's Dev Tools
+    :return: python compatible code in a list
     """
     # remove quotations
     args = shlex.split(command)
@@ -66,6 +91,7 @@ def curlToPython(command):
             myargs = args[i + 1].split(':')
             headers[myargs[0]] = ''.join(myargs[1:]).strip()
 
+    cookies = {}
     # gather all the cookies
     if 'Cookie' in headers:
         cookie = headers['Cookie']
@@ -73,18 +99,18 @@ def curlToPython(command):
         del headers['Cookie']
         cookies = dict([c.strip().split('=') for c in cookie.split(';')])
 
-    pycode = []
-    pycode.append("#!/usr/bin/env python")
-    pycode.append("import requests\n")
-    pycode += create_request(url, data, method, cookies, headers)
-    return pycode
+    code = []
+    code.append("#!/usr/bin/env python")
+    code.append("import requests\n")
+    code += create_request(url, method, cookies, headers, data)
+    return code
 
 
-def resToCurl(res):
+def res_to_curl(res):
     """converts a requests response to a curl command
 
     >>> res = requests.get('http://www.example.com')
-    >>> print resToCurl(res)
+    >>> print res_to_curl(res)
     curl 'http://www.example.com/' -X 'GET' ...
 
     Source: http://stackoverflow.com/a/17936634
@@ -93,26 +119,31 @@ def resToCurl(res):
     """
     req = res.request
     command = "curl '{uri}' -X '{method}' -H {headers}"
-    headers = ["{0}: {1}".format(k, v) for k, v in req.headers.items()]
-    headerLine = " -H ".join(['"{0}"'.format(h) for h in headers])
-    if req.method == 'GET':
-        return command.format(method=req.method, headers=headerLine,
+    headers = ["{}: {}".format(k, v) for k, v in req.headers.items()]
+    header_line = " -H ".join(['"{}"'.format(h) for h in headers])
+    if req.method == "GET":
+        return command.format(method=req.method, headers=header_line,
                               uri=req.url)
     else:
         command += " --data-binary '{data}'"
-        return command.format(method=req.method, headers=headerLine,
+        return command.format(method=req.method, headers=header_line,
                               data=req.body, uri=req.url)
 
 
 def main():
+    """Main entry point.
+
+    Purposely didn't use argparse or another command line parser to keep this
+    script simple.
+    """
     if len(sys.argv) == 1:
-        command = 'curl "http://localhost:8080/api/v1/test" ' + \
+        command = 'curl "http://www.example.com" ' + \
                   '-H "Pragma: no-cache" ' + \
                   '-H "Accept-Encoding: gzip, deflate" ' + \
                   '-H "Accept-Language: en-US,en;q=0.8"'
     else:
         command = sys.argv[1]
-    code = curlToPython(command)
+    code = curl_to_python(command)
     print('\n'.join(code))
 
 
