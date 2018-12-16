1173a5e99805f842704e768cbba6c2caacf9eaeb
nitro <nitrocode@users.noreply.github.com>
2018-12-16 16:33:56 -0500

    Packaged up and pushed on to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='curltopy',
+    version='0.1.1',
+    author='',
+    author_email='',
+    description='Converts curl statements to python using command line',
+    url="https://github.com/nitrocode/curltopy",
+    packages=['curltopy'],
+    scripts=['bin/curltopy']
+)
