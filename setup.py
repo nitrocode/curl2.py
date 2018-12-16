61185eb49975261707c0056aef886b5c04c483c0
nitro <nitrocode@users.noreply.github.com>
2018-12-16 17:41:41 -0500

    Added longdescription to package

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
     name='curltopy',
-    version='0.1.1',
+    version='0.1.2',
     author='',
     author_email='',
     description='Converts curl statements to python using command line',
+    long_description=open('README.md').read(),
     url="https://github.com/nitrocode/curl2.py",
     packages=['curltopy'],
     scripts=['bin/curltopy']
